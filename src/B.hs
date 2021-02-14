c54b11c0ddcd115a3a5f999e7a471ef9b5a86d53
Christoffer Kaltenbrunner <47105299+kaltenbrunner@users.noreply.github.com>
2021-02-14 22:43:48 +0100

    Add example files for requirement 1

---
+++ src/B.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE MonadComprehensions #-}
+
+module B where
+
+import Database.Daison
+import System.Environment
+
+people_name :: Index (String,Int) String
+people_name = index people "name" fst
+
+people :: Table (String,Int)
+people = table "people"
+           `withIndex` people_name
+
+main = do
+  db <- openDB "test.db"
+  x <- runDaison db ReadWriteMode $ do
+         --dropTable people
+         tryCreateTable people
+         insert people (return ("Aga",15))
+         insert people (return ("Aga",15))
+         --update people2 (\_ (name,age,x) -> (name,age,10))
+           --             (from people2)
+         select [x | x <- from people everything]
+  print x
+  closeDB db
+
