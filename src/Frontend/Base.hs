8f877754b9319e2d9c3921d42e37a401e4ef491e
pugzera <alex.neldefors@gmail.com>
2021-02-15 18:21:30 +0100

    Moved everything into the same module

--- src/Frontend/Base.hs
+++ src/Frontend/Base.hs
@@ -1,5 +1,4 @@
 module Base (
-  runGhc,
   preludeModuleName,
   daisonModuleName
 ) where
@@ -8,17 +7,48 @@ import qualified GHCInterface as GHC
 
 import Database.Daison
 
+import qualified Control.Exception as E
+
 instance GHC.HasDynFlags Daison where
+    getDynFlags = GHC.getDynFlags
 
 instance GHC.ExceptionMonad Daison where
+    gcatch = GHC.gcatch
+    gmask = GHC.gmask
 
 instance GHC.GhcMonad Daison where
   getSession = GHC.getSession
   setSession = GHC.setSession
 
+
+
 preludeModuleName, daisonModuleName :: GHC.ModuleName
 preludeModuleName = GHC.mkModuleName "Prelude"
 daisonModuleName  = GHC.mkModuleName "Database.Daison"
 
-runGhcDaison :: Daison a -> IO a
+--runGhcDaison :: GHC.GhcMonad a -> IO a
 runGhcDaison = GHC.runGhc (Just GHC.libdir)
+
+loadModules :: GHC.GhcMonad m => [GHC.InteractiveImport] -> m ()
+loadModules is = do
+  ctx <- GHC.getContext
+  GHC.setContext (is ++ ctx)
+
+makeIIModule :: GHC.ModuleName -> GHC.InteractiveImport
+makeIIModule = GHC.IIModule
+
+makeIIDecl :: GHC.ModuleName -> GHC.InteractiveImport
+makeIIDecl = GHC.IIDecl . GHC.simpleImportDecl
+
+runStmt :: String -> Daison (Maybe GHC.ExecResult)
+runStmt stmt = do
+      dflags <- GHC.getSessionDynFlags
+      GHC.setSessionDynFlags dflags
+
+      loadModules $ map makeIIDecl [preludeModuleName, daisonModuleName]
+
+      res <- GHC.execStmt stmt GHC.execOptions
+      return $ case res of
+        GHC.ExecComplete {GHC.execResult = Right _} -> Just res
+        GHC.ExecComplete {GHC.execResult = Left e}  -> E.throw e
+        _                                           -> Nothing
