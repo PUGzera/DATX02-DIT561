200d259c726101ed78a040a5d99ad5651094421c
pugzera <alex.neldefors@gmail.com>
2021-02-16 18:13:05 +0100

    Added state and a type DaisonI for computations with the state

--- src/Frontend/Base.hs
+++ src/Frontend/Base.hs
@@ -9,28 +9,26 @@ import Database.Daison
 
 import qualified Control.Exception as E
 
-import Data.IORef
+import qualified Control.Monad.Trans.State.Lazy as St
 
 
 data DaisonState = DaisonState {
     mode :: AccessMode,
-    db :: Database
+    db :: String
 }
 
-type DaisonStateRef = IORef DaisonState
-
-data DaisonI a = DaisonI { unwrap :: DaisonStateRef -> GHC.Ghc a }
+data DasionI a = DaisonI { exec :: DaisonState -> GHC.Ghc a }
 
 preludeModuleName, daisonModuleName :: GHC.ModuleName
 preludeModuleName = GHC.mkModuleName "Prelude"
 daisonModuleName  = GHC.mkModuleName "Database.Daison"
 
 
-runGhc :: GHC.Ghc a -> IO a
-runGhc = GHC.runGhc (Just GHC.libdir)
+runGhc :: DaisonState -> DasionI a -> IO a
+runGhc state ds = GHC.runGhc (Just GHC.libdir) ((exec ds) state)
 
-loadModules :: [GHC.InteractiveImport] -> GHC.Ghc ()
-loadModules is = do
+loadModules :: [GHC.InteractiveImport] -> DasionI ()
+loadModules is = DaisonI $ \st -> do
   ctx <- GHC.getContext
   GHC.setContext (is ++ ctx)
 
@@ -40,12 +38,12 @@ makeIIModule = GHC.IIModule
 makeIIDecl :: GHC.ModuleName -> GHC.InteractiveImport
 makeIIDecl = GHC.IIDecl . GHC.simpleImportDecl
 
-runStmt :: String -> GHC.Ghc (Maybe GHC.ExecResult)
-runStmt stmt = do
+runStmt :: String -> DasionI (Maybe GHC.ExecResult)
+runStmt stmt = DaisonI $ \st -> do
       dflags <- GHC.getSessionDynFlags
       GHC.setSessionDynFlags dflags
       GHC.liftIO $ print "step 1"
-      loadModules $ map makeIIDecl [preludeModuleName, daisonModuleName]
+      exec (loadModules $ map makeIIDecl [preludeModuleName, daisonModuleName]) st
       GHC.liftIO $ print "step 2"
       res <- GHC.execStmt stmt GHC.execOptions
       GHC.liftIO $ print "step 3"
@@ -54,3 +52,7 @@ runStmt stmt = do
         GHC.ExecComplete {GHC.execResult = Left e}  -> E.throw e
         _                                           -> Nothing
 
+main :: IO ()
+main = do
+    runGhc (DaisonState ReadWriteMode "hej") (runStmt "openDB \"hej.db\"")
+    return ()
