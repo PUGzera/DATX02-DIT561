3550cb20800eab3d2e510c93914b1598c4b5ffaf
pugzera <alex.neldefors@gmail.com>
2021-02-17 13:13:48 +0100

    Added function to read queries, not fully implemented yet

--- src/Frontend/Eval.hs
+++ src/Frontend/Eval.hs
@@ -11,6 +11,8 @@ import Prelude
 import Base
 import Context
 
+import Database.Daison
+
 -- | Run statements from Prelude and Daison in the 'DaisonI' monad.
 runStmt :: String -> DaisonI (Maybe GHC.ExecResult)
 runStmt stmt = DaisonI $ \st -> do
@@ -21,6 +23,13 @@ runStmt stmt = DaisonI $ \st -> do
 
       res <- GHC.execStmt stmt GHC.execOptions
       return $ case res of
-        GHC.ExecComplete {GHC.execResult = Right _} -> Just res
+        GHC.ExecComplete {GHC.execResult = Right _} -> (Just res, st)
         GHC.ExecComplete {GHC.execResult = Left e}  -> E.throw e
-        _                                           -> Nothing
+        _                                           -> (Nothing, st)
+
+readQuery :: DaisonI ()
+readQuery = do
+    st <- getState --use to get db and access mode later
+    query <- GHC.liftIO getLine
+    res <- runStmt query
+    return ()
