baf1d001787281fdc7bca35cd7d5fac409b96742
Christoffer Kaltenbrunner <47105299+kaltenbrunner@users.noreply.github.com>
2021-02-25 20:05:00 +0100

    Add function for typechecking expressions

--- src/Frontend/GHCInterface.hs
+++ src/Frontend/GHCInterface.hs
@@ -9,6 +9,7 @@ import GHC as X
 import GHC.Paths as X (libdir) -- maybe put this in another module since ghc-paths is a package
 import DynFlags as X
 import Outputable as X
+import PprTyThing as X
 import GhcMonad as X
 import GHC.LanguageExtensions as X
 
