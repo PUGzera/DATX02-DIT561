48ffa85f1a99bacd77ba4fcf4dc7193b18da8b09
pvedin <pvedin@yahoo.se>
2021-03-03 22:57:53 +0100

    Multiple databases
    Databases are opened when the user uses the ":db" or ":open" commands, and are closed when the user either uses the ":close" or the ":quit" command.
    Errors are displayed if the user tries to run a Daison statement without first opening a database, or if they try to close a database which has not yet been opened.

--- src/Main.hs
+++ src/Main.hs
@@ -29,17 +29,24 @@ run = do
     this <- myThreadId
     installHandler keyboardSignal (Catch (GHC.throwTo this GHC.UserInterrupt)) Nothing
 
-    state <- return $ DaisonState ReadWriteMode "test.db" [] Nothing
+    state <- return $ DaisonState ReadWriteMode "" [] [] Nothing
     runGhc state $ do
         initSession
         loop 
     return ()
 
+
+{- Within the session:
+   _activeDB  :: Database
+   _openDBs   :: [(String, Database)]
+-}
 initSession :: DaisonI ()
 initSession = do
     dflags <- liftGhc GHC.getSessionDynFlags
     liftGhc $ GHC.setSessionDynFlags dflags
     loadModules $ map makeIIDecl [preludeModuleName, daisonModuleName, ioClassModuleName]
+
+    runStmt $ "let _openDBs = [] :: [(String, Database)]"
     return ()
 
 loop :: DaisonI ()
@@ -47,34 +54,97 @@ loop = do
     state <- getState
     let settings = defaultSettings {historyFile = Just "daison_history"}
 
-    res <- GHC.liftIO $ runInputT settings $ getInputLine $ "Daison (" ++ db state ++ ")> "
+    res <- GHC.liftIO $ runInputT settings $ getInputLine $ getPrompt state
+
     do
     case res of
-        Nothing      -> return ()
+        Nothing      -> cmdQuit
         Just ""      -> loop
-        Just ":q"    -> return ()
-        Just ":quit" -> return ()
+        Just ":dbs"  -> cmdListOpenDBs
+        Just ":q"    -> cmdQuit
+        Just ":quit" -> cmdQuit
         Just input 
-            | ":db "     `isPrefixOf` input -> cmdDB input
+            | ":close "  `isPrefixOf` input -> cmdClose input
+            | ":db "     `isPrefixOf` input -> cmdOpen input
             | ":import " `isPrefixOf` input -> cmdImport input
+            | ":open "   `isPrefixOf` input -> cmdOpen input
             | ":t "      `isPrefixOf` input -> cmdType input
             | otherwise                     -> cmdStmt input            
     `catch`
         handleError state
 
+getPrompt :: DaisonState -> String
+getPrompt state = do
+    case activeDB state of
+        ""   -> "Daison> "
+        file -> "Daison (" ++ file ++ ")> "
+
 removeCmd :: String -> String
 removeCmd = unwords . tail . words
 
-cmdDB :: String -> DaisonI ()
-cmdDB input = do
-    modifyState $ \st -> st {db = removeCmd input}
+removeDoubleQuotes :: String -> String
+removeDoubleQuotes = filter (\ch -> ch /= '"')
+
+cmdQuit :: DaisonI ()
+cmdQuit = do
+    state <- getState
+    sequence_ $ map (\database -> runStmt (sCloseDB database)) (openDBs state)
+
+cmdListOpenDBs :: DaisonI ()
+cmdListOpenDBs = do
+    state <- getState
+    GHC.liftIO $ print $ openDBs state
     loop
-    
+
+-- | Opens a database within the session and marks it as active,
+--   while keeping track of other open databases.
+--   Already opened databases will not be reopened.
+cmdOpen :: String -> DaisonI ()
+cmdOpen input = do
+    state <- getState
+    let arg = removeDoubleQuotes $ (words input) !! 1
+    let dbs = openDBs state
+    runStmt $ "_activeDB <- openDB \"" ++ arg ++ "\""
+    case arg `elem` dbs of
+        True -> do
+            modifyState $ \st -> st{activeDB = arg}
+        False -> do
+            updateSessionVariable "_openDBs" $ sAddDB arg
+            runStmt $ "print $ length _openDBs"
+            modifyState $ \st -> st{activeDB = arg,
+                                    openDBs = arg : dbs}
+    loop
+
+-- | Closes a database within the session.
+--   If this database was the active one, also marks the earliest opened
+--   database as active.
+--   Throws an exception if the database has not been opened.
+cmdClose :: String -> DaisonI ()
+cmdClose input = do
+    state <- getState
+    let arg = removeDoubleQuotes $ (words input) !! 1
+    let dbs = openDBs state
+    case arg `elem` dbs of
+        True -> do
+            let dbs' = filter (\str -> str /= arg) dbs
+            let newActive = case dbs' of
+                    [] -> ""
+                    _ -> head dbs'
+            runStmt $ sCloseDB arg 
+            updateSessionVariable "_openDBs" $ sRemoveDB arg
+            runStmt $ "let _activeDB = " ++ sGetDB newActive
+            runStmt $ "print $ length _openDBs"
+            modifyState $ \st -> st{activeDB = newActive,
+                                    openDBs = dbs'}
+            loop
+        False -> GHC.throw DBNotOpen
+
 cmdImport :: String -> DaisonI ()
 cmdImport input = do
     addImport' $ removeCmd input -- TODO: load modules here.
                                  -- This throws an error now. Also, it doesn't handle the case when input is ""
     loop
+
 cmdType :: String -> DaisonI ()
 cmdType input = do
     let arg = removeCmd input
@@ -84,16 +154,23 @@ cmdType input = do
 
 cmdStmt :: String -> DaisonI ()
 cmdStmt stmt = do
-    state <- getState
-    let ts = "do {db <- openDB \"" ++ db state ++ "\"; " 
-                ++ "res <- runDaison db " ++ show (mode state) 
-                ++ "(do " -- ++ stmt
-    let tf =  "); closeDB db}"
     isQuery <- exprIsQuery stmt
     case isQuery of
-        True -> runStmt $ ts ++ stmt ++ tf
+        True -> runDaisonStmt stmt -- TODO: Find a way to read the result outside of the session
         False -> runStmt stmt
     loop
+
+-- | Perform a Daison transaction.
+--   Throws an exception if no database has been opened.
+runDaisonStmt :: String -> DaisonI (Maybe GHC.ExecResult)
+runDaisonStmt stmt = do
+    state <- getState
+    let query = "runDaison _activeDB " 
+                ++ show (mode state) ++ " " 
+                ++ "$ (" ++ stmt ++ ")"
+    case activeDB state of
+        "" -> GHC.throw NoOpenDB
+        _ -> runStmt query 
 
 handleError :: DaisonState -> GHC.SomeException -> DaisonI ()
 handleError state e = do 
@@ -104,3 +181,44 @@ handleError state e = do
         \e -> do
             GHC.liftIO $ print $ (e :: GHC.AsyncException)
             return ()
+
+-- | Used as a workaround to redefine a variable
+--   in terms of itself
+updateSessionVariable :: String -> String -> DaisonI ()
+updateSessionVariable var newValue = do
+    runStmt $ "let temp_" ++ var ++ " = " ++ newValue
+    runStmt $ "let " ++ var ++ " = temp_" ++ var
+    return () 
+
+{- Functions to be used as part of runStmt arguments -}
+
+-- | Within session: Database
+--   Opens a database.
+sOpenDB :: String -> String
+sOpenDB fileName = "openDB \"" ++ fileName ++ "\""
+
+-- | Within session: IO ()
+--   Closes a database.
+sCloseDB :: String -> String
+sCloseDB fileName = "closeDB $ " ++ sGetDB fileName
+
+-- | Within session: Database
+--   Returns a Database from the list of opened databases.
+sGetDB :: String -> String
+sGetDB fileName = "snd . head $ filter (\\(x,_) -> x == \"" ++ fileName ++ "\") _openDBs"
+
+-- | Within session: [(String, Database)]
+--   Add the active database to a list of opened databases.
+--   Make sure to do sOpenDB first!
+sAddDB :: String -> String
+sAddDB fileName = "(\"" ++ fileName ++ "\", _activeDB) : _openDBs"
+
+-- | Within session: [(String, Database)]
+--   Removes a database from the list of opened databases.
+--   Make sure to do sCloseDB first!
+sRemoveDB :: String -> String
+sRemoveDB fileName = "filter (\\(x,_) -> x /= \"" ++ fileName ++ "\") _openDBs"
+
+
+
+
