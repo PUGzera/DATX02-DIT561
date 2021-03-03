d8c921f55f0fcf606221e12bd0683381992c5638
pvedin <pvedin@yahoo.se>
2021-03-03 03:04:35 +0100

    Code refactoring
    - `main` is now synonymous with `run`
    -  `initSession` is run before `loop` (containing the lines removed from `runStmt` in an earlier commit)
    -  Type signatures with `DaisonI (Maybe GHC.ExecResult, Nothing)` now instead contain `DaisonI ()`
    - The `case res of` statement should now be more readable

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,7 @@ import Frontend.Eval
 import qualified Frontend.GHCInterface as GHC
 
 import Database.Daison
-import System.Console.Haskeline
+import System.Console.Haskeline hiding (display)
 
 import Data.List
 import Control.Monad.Catch (catch)
@@ -19,50 +19,72 @@ instance Show AccessMode where
     show ReadWriteMode = "ReadWriteMode"
     show ReadOnlyMode = "ReadOnlyMode"
 
--- | Ensure run is not in a half-active state after CTRL+C when run in GHCi
 main :: IO ()
-main = do
-    this <- myThreadId
-    installHandler keyboardSignal (Catch (GHC.throwTo this GHC.UserInterrupt)) Nothing
-    run
+main = run
 
 run :: IO ()
 run = do
+    -- Ensure run is not in a half-active state after CTRL+C when run in GHCi
+    this <- myThreadId
+    installHandler keyboardSignal (Catch (GHC.throwTo this GHC.UserInterrupt)) Nothing
+
     state <- return $ DaisonState ReadWriteMode "test.db" [] Nothing
-    runGhc state loop 
+    runGhc state $ do
+        initSession
+        loop 
     return ()
 
-loop :: DaisonI (Maybe GHC.ExecResult, DaisonState)
+initSession :: DaisonI ()
+initSession = do
+    dflags <- liftGhc GHC.getSessionDynFlags
+    liftGhc $ GHC.setSessionDynFlags dflags
+    loadModules $ map makeIIDecl [preludeModuleName, daisonModuleName, ioClassModuleName]
+    return ()
+
+loop :: DaisonI ()
 loop = do
     state <- getState
-
     let settings = defaultSettings {historyFile = Just "daison_history"}
-    let ts = "do {db <- openDB \"" ++ db state ++ "\"; " 
-                ++ "res <- runDaison db " ++ show (mode state) 
-                ++ "(do " -- ++ stmt
-    let tf =  ");  closeDB db;}"
 
     res <- GHC.liftIO $ runInputT settings $ getInputLine $ "Daison (" ++ db state ++ ")> "
     do
-        case res of
-            Nothing -> return (Nothing, state)
-            Just "" -> loop
-            Just "q" -> return (Nothing, state)
-            Just "quit" -> return (Nothing, state)
-            Just input | "db " `isPrefixOf` input -> do
-                modifyState $ \st -> st {db = words input !! 1}
-                loop
-            Just input | "import " `isPrefixOf` input -> do
-                addImport' (words input !! 1) -- TODO: load modules here.
-                                              -- This throws an error now. Also, it doesn't handle the case when input is ""
-                loop
-            Just stmt -> do
-                runStmt $ ts ++ stmt ++ tf
-                loop
-        `catch`
+    case res of
+        Nothing      -> return ()
+        Just ""      -> loop
+        Just ":q"    -> return ()
+        Just ":quit" -> return ()
+        Just input 
+            | ":db "     `isPrefixOf` input -> cmdDB input
+            | ":import " `isPrefixOf` input -> cmdImport input
+            | otherwise                     -> cmdStmt input            
+    `catch`
         handleError state
 
-handleError :: DaisonState -> GHC.SomeException -> DaisonI (Maybe GHC.ExecResult, DaisonState)
+removeCmd :: String -> String
+removeCmd = unwords . tail . words
+
+cmdDB :: String -> DaisonI ()
+cmdDB input = do
+    modifyState $ \st -> st {db = removeCmd input}
+    loop
+    
+cmdImport :: String -> DaisonI ()
+cmdImport input = do
+    addImport' $ removeCmd input -- TODO: load modules here.
+                                 -- This throws an error now. Also, it doesn't handle the case when input is ""
+    loop
+
+cmdStmt :: String -> DaisonI ()
+cmdStmt stmt = do
+    state <- getState
+    let ts = "do {db <- openDB \"" ++ db state ++ "\"; " 
+                ++ "res <- runDaison db " ++ show (mode state) 
+                ++ "(do " -- ++ stmt
+    let tf =  "); closeDB db}"
+    runStmt $ ts ++ stmt ++ tf
+    loop
+
+handleError :: DaisonState -> GHC.SomeException -> DaisonI ()
 handleError state e = do 
         do
             GHC.liftIO $ print (e :: GHC.SomeException)
@@ -70,4 +92,4 @@ handleError state e = do
         `catch` 
         \e -> do
             GHC.liftIO $ print $ (e :: GHC.AsyncException)
-            return (Nothing, state)
+            return ()
