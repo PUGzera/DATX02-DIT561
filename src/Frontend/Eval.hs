PUGzera/DATX02-DIT561

commit 7fb19728c6fb63e0fd97afb9537e91d39c593d40

Added support for declarations
Declarations are expressions such as "data Bool = False | True", "x = 10".
Writing data types using record syntax is also supported.

src/Frontend/Eval.hs
@@ -1,6 +1,6 @@
 module Frontend.Eval (
   display,
-  runStmt
+  runExpr
 ) where
 
 import qualified Frontend.GHCInterface as GHC
@@ -12,6 +12,7 @@ import Prelude
 
 import Frontend.Base
 import Frontend.Context
+import Frontend.Typecheck
 
 
 -- | Send string to the 'less' command via the 'echo' command, making it 
@@ -34,11 +35,22 @@ display showable = do
         Left _ -> putStrLn . show $ showable
         Right () -> return ()
 
--- | Run statements from Prelude and Daison in the 'DaisonI' monad.
-runStmt :: String -> DaisonI (Maybe GHC.ExecResult)
+-- | Run expressions in the DaisonI monad.
+runExpr :: String -> DaisonI [GHC.Name]
+runExpr expr = do
+    category <- getExprCategory expr
+    case category of
+        Just "Statement"   -> runStmt expr
+        Just "Declaration" -> runDecl expr
+        _                  -> return []
+
+-- | Run declarations in the DaisonI monad.
+runDecl :: String -> DaisonI [GHC.Name]
+runDecl = liftGhc . GHC.runDecls
+
+-- | Run statements in the DaisonI monad.
+runStmt :: String -> DaisonI [GHC.Name]
 runStmt stmt = do
-      res <- liftGhc $ GHC.execStmt stmt GHC.execOptions
-      return $ case res of
-        GHC.ExecComplete {GHC.execResult = Right _} -> (Just res)
-        GHC.ExecComplete {GHC.execResult = Left e}  -> E.throw e
-        _                                           -> Nothing
+    res <- liftGhc $ GHC.execStmt stmt GHC.execOptions
+    let Right names = GHC.execResult res
+    return names

src/Frontend/Base.hs
@@ -5,6 +5,7 @@ module Frontend.Base (
   preludeModuleName,
   daisonModuleName,
   ioClassModuleName,
+  dataModuleName,
   runGhc,
   getState,
   modifyState,
@@ -103,6 +104,7 @@ preludeModuleName, daisonModuleName :: GHC.ModuleName
 preludeModuleName = GHC.mkModuleName "Prelude"
 daisonModuleName  = GHC.mkModuleName "Database.Daison"
 ioClassModuleName = GHC.mkModuleName "Control.Monad.IO.Class"
+dataModuleName = GHC.mkModuleName "Data.Data"
 
 runGhc :: DaisonState -> DaisonI a -> IO (a, DaisonState)
 runGhc state ds = GHC.runGhc (Just GHC.libdir) ((exec ds) state)

src/Frontend/Typecheck.hs
@@ -1,6 +1,7 @@
 module Frontend.Typecheck (
     exprType,
     exprIsQuery,
+    getExprCategory,
     mToDaison
 ) where
 
@@ -25,37 +26,54 @@ exprType expr = do
 -- | Check if an expression is a Daison query
 exprIsQuery :: String -> DaisonI Bool
 exprIsQuery expr = do
-    t <- catch (exprType expr) ignoreAssignmentError
+    t <- catch (exprType expr) errorHandler
     let t' = removeTypeConstraint t
 
-    case Just t' >>= startsWithLowerM >>= isDaison of
-        Nothing -> return True  -- run in Daison monad
-        Just t'  -> return False -- run as pure/IO expression
+    if t' == ignoredError
+    then return False
+    else return $ startsWithLowerM t' || isDaison t'
 
     where
-        -- Assumes that "let a = b" is the only reasonable input that
-        -- raises the "not an expression" error
-        ignoreAssignmentError :: GHC.SourceError -> DaisonI String
-        ignoreAssignmentError e = do
-            let msg = show e
-            case take 18 msg of
-                "not an expression:" -> return msg
-                _ -> GHC.liftIO $ GHC.throwIO e
+        ignoredError = "_ IgnoredError"
+        errorHandler :: GHC.SourceError -> DaisonI String
+        errorHandler e = do
+            case Just e >>= isAssignmentError >>= possibleDeclaration of
+                Nothing -> return ignoredError
+                Just e  -> GHC.liftIO $ GHC.throwIO e
 
-        startsWithLowerM :: String -> Maybe String
-        startsWithLowerM ('m':str) = Nothing
-        startsWithLowerM str       = Just str
+        -- | Assumes that "let a = b" is the only reasonable input that
+        --   raises the "not an expression" err
+        isAssignmentError :: GHC.SourceError -> Maybe GHC.SourceError
+        isAssignmentError e
+            | "not an expression:" `isPrefixOf` msg = Nothing
+            | otherwise                             = Just e
+            where msg = show e
 
-        isDaison :: String -> Maybe String
-        isDaison str
-            | take 6 str == "Daison" = Nothing
-            | otherwise              = Just str
+        startsWithLowerM :: String -> Bool
+        startsWithLowerM = ("m" `isPrefixOf`)
+
+        isDaison :: String -> Bool
+        isDaison = ("Daison" `isPrefixOf`)
+
+getExprCategory :: String -> DaisonI (Maybe String)
+getExprCategory expr = do
+    t <- catch (Right <$> (exprType expr)) (\e -> return (Left (possibleDeclaration e)))
+    case t of
+        Left  Nothing  -> return $ Just "Declaration"
+        Left  _        -> return $ Just "Statement"
+        Right _        -> return $ Just "Statement"
+
+possibleDeclaration :: GHC.SourceError -> Maybe GHC.SourceError
+possibleDeclaration e
+    | "parse error on input" `isPrefixOf` msg = Nothing
+    | otherwise                               = Just e
+    where msg = show e
 
 -- | Tell the interpreter to parse 'm a' as 'Daison a'.
 mToDaison :: String -> DaisonI String
 mToDaison stmt = do
         t <- exprType stmt
-        return $ "(" ++ stmt ++ ") :: " ++ (asDaison . removeTypeConstraint) t
+        return $ "(" ++ stmt ++ ") :: " ++ (asDaison . removeTypeConstraint) t  
         where
             asDaison ('m':t) = "Daison" ++ t
             asDaison t = t

src/Main.hs
@@ -49,9 +49,10 @@ initSession :: DaisonI ()
 initSession = do
     dflags <- liftGhc GHC.getSessionDynFlags
     liftGhc $ GHC.setSessionDynFlags dflags
-    mapM_ addImport $ map makeIIDecl [preludeModuleName, daisonModuleName, ioClassModuleName]
+    mapM_ addImport $ map makeIIDecl [preludeModuleName, daisonModuleName, ioClassModuleName, dataModuleName]
     addExtension GHC.MonadComprehensions
-    runStmt $ "let _openDBs = [] :: [(String, Database)]"
+    addExtension GHC.DeriveDataTypeable
+    runExpr $ "let _openDBs = [] :: [(String, Database)]"
     return ()
 
 loop :: DaisonI ()
@@ -74,7 +75,7 @@ loop = do
             | ":open "   `isPrefixOf` input -> cmdOpen input
             | ":t "      `isPrefixOf` input -> cmdType input
             | ":import"  `isPrefixOf` input -> cmdImport input
-            | otherwise                     -> cmdStmt input
+            | otherwise                     -> cmdExpr input
         `catch`
             handleError state
 
@@ -93,7 +94,7 @@ removeDoubleQuotes = filter (\ch -> ch /= '"')
 cmdQuit :: DaisonI ()
 cmdQuit = do
     state <- getState
-    sequence_ $ map (\database -> runStmt (sCloseDB database)) $ openDBs state
+    sequence_ $ map (\database -> runExpr (sCloseDB database)) $ openDBs state
 
 cmdListOpenDBs :: DaisonI ()
 cmdListOpenDBs = do
@@ -109,7 +110,7 @@ cmdOpen input = do
     state <- getState
     let arg = removeDoubleQuotes $ (words input) !! 1
     let dbs = openDBs state
-    runStmt $ "_activeDB <- openDB \"" ++ arg ++ "\""
+    runExpr $ "_activeDB <- openDB \"" ++ arg ++ "\""
     case arg `elem` dbs of
         True -> do
             modifyState $ \st -> st{activeDB = Just arg}
@@ -134,9 +135,9 @@ cmdClose input = do
             let newActive = case dbs' of
                     [] -> Nothing
                     _ -> Just $ head dbs'
-            runStmt $ sCloseDB arg 
+            runExpr $ sCloseDB arg 
             updateSessionVariable "_openDBs" $ sRemoveDB arg
-            runStmt $ "let _activeDB = " ++ sGetDB newActive
+            runExpr $ "let _activeDB = " ++ sGetDB newActive
             modifyState $ \st -> st{activeDB = newActive,
                                     openDBs = dbs'}
             loop
@@ -154,17 +155,17 @@ cmdType input = do
     GHC.liftIO $ putStrLn $ arg ++ " :: " ++ t
     loop
 
-cmdStmt :: String -> DaisonI ()
-cmdStmt stmt = do
-    isQuery <- exprIsQuery stmt
+cmdExpr :: String -> DaisonI ()
+cmdExpr expr = do
+    isQuery <- exprIsQuery expr
     case isQuery of
-        True -> runDaisonStmt stmt -- TODO: Find a way to read the result outside of the session
-        False -> runStmt stmt
+        True -> runDaisonStmt expr -- TODO: Find a way to read the result outside of the session
+        False -> runExpr expr
     loop
 
 -- | Perform a Daison transaction.
 --   Throws an exception if no database has been opened.
-runDaisonStmt :: String -> DaisonI (Maybe GHC.ExecResult)
+runDaisonStmt :: String -> DaisonI [GHC.Name]
 runDaisonStmt stmt = do
     state <- getState
     t <- exprType stmt
@@ -174,7 +175,7 @@ runDaisonStmt stmt = do
                 ++ "$ (" ++ daisonStmt ++ ")"
     case activeDB state of
         Nothing -> GHC.throw NoOpenDB
-        Just _  -> runStmt query
+        Just _  -> runExpr query
 
 handleError :: DaisonState -> GHC.SomeException -> DaisonI ()
 handleError state e = do 
@@ -190,11 +191,11 @@ handleError state e = do
 --   in terms of itself
 updateSessionVariable :: String -> String -> DaisonI ()
 updateSessionVariable var newValue = do
-    runStmt $ "let temp_" ++ var ++ " = " ++ newValue
-    runStmt $ "let " ++ var ++ " = temp_" ++ var
+    runExpr $ "let temp_" ++ var ++ " = " ++ newValue
+    runExpr $ "let " ++ var ++ " = temp_" ++ var
     return () 
 
-{- Functions to be used as part of runStmt arguments -}
+{- Functions to be used as part of runExpr arguments -}
 
 -- | Within session: Database
 --   Opens a database.

commit 329501a39c61f484107ac73e10e1bc5ea3cd16d2

Fix #16 + removed redundant import
The bug mentioned in issue #16 was introduced by commit 7fb19728c6fb63e0fd97afb9537e91d39c593d40, which accidentally removed the handling of the case where GHC.execStmt returns a Left execResult without readding it.

src/Frontend/Eval.hs
@@ -5,7 +5,6 @@ module Frontend.Eval (
 
 import qualified Frontend.GHCInterface as GHC
 
-import qualified Control.Exception as E
 import qualified System.Process as P
 
 import Prelude
@@ -29,8 +28,8 @@ display showable = do
             P.waitForProcess hcmd
             return $ Right ()
 
-    res <- GHC.liftIO $ E.catch sendToLess $
-            \e -> (return . Left . show) (e :: E.IOException)
+    res <- GHC.liftIO $ GHC.catch sendToLess $
+            \e -> (return . Left . show) (e :: GHC.IOException)
     GHC.liftIO $ case res of
         Left _ -> putStrLn . show $ showable
         Right () -> return ()
@@ -52,5 +51,6 @@ runDecl = liftGhc . GHC.runDecls
 runStmt :: String -> DaisonI [GHC.Name]
 runStmt stmt = do
     res <- liftGhc $ GHC.execStmt stmt GHC.execOptions
-    let Right names = GHC.execResult res
-    return names
+    case GHC.execResult res of
+        Left error  -> GHC.throw error
+        Right names -> return names