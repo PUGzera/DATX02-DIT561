b124d71cd3c8482768dd76332320943df718f50d
pvedin <pvedin@yahoo.se>
2021-04-21 23:30:50 +0200

    Resolve #36 + renamed `daison_history`
    `daison_history` is now `.daison_history` and can be found at
    `~/.Daison-Frontend/.daison_history`
    on Unix-like systems, and
    ` %APPDATA%/.Daison-Frontend/.daison_history`
    on Windows systems.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,19 @@
 module Main (
   main
 ) where
+
+import qualified Frontend.GHCInterface as GHC
 import Frontend.Run (run)
+
 import System.Console.Haskeline
+import System.Directory
 
-settings = defaultSettings {historyFile = Just "daison_history"}
+folderName = "Daison-Frontend"
+logName = ".daison_history"
 
-main = run $ runInputT settings . getInputLine 
+main = do
+    dataPath <- getAppUserDataDirectory folderName
+    createDirectoryIfMissing True dataPath
+    let fullPath = dataPath ++ '/' : logName
+    let settings = defaultSettings {historyFile = Just fullPath}
+    run $ runInputT settings . getInputLine 
