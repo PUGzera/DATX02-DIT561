e1de66fe9cf6da3cab0c33608beb55e336c3f532
pvedin <pvedin@yahoo.se>
2021-05-14 22:45:33 +0200

    Made the read test more general
    The number of iterations have been returned to the default value (100 instead of 10).
    The test is now also done four times, with different data types.

--- test/Tests.hs
+++ test/Tests.hs
@@ -4,7 +4,7 @@ module Main (main) where
     
 import Frontend.Run
 
-import Database.Daison
+import Database.Daison hiding (tableName, createTable)
 import Test.QuickCheck
 import Test.QuickCheck.Test
 
@@ -18,14 +18,16 @@ import System.Exit
 
 --  Test data types
 
-data People = People {name :: String, age :: Int} deriving (Data, Show, Eq)
+data Person = Person {name :: String, age :: Int} deriving (Data, Show)
+instance Arbitrary Person where
+    arbitrary = applyArbitrary2 $ \s i -> Person s i
 
-tPeople :: Table People
-tPeople = table "People" `withIndex` iPeopleName
+-- Test values
+tableName :: String
+tableName = "testTable"
 
-iPeopleName  :: Index People String
-iPeopleName = index tPeople "Name" name
-
+testTable :: Data a => Table a -- needs type casting
+testTable = table tableName
 
 -- Functions for interacting with the front-end
 
@@ -42,6 +44,11 @@ getNextInput inputRef _logInput _prompt = do
 
 
 -- Helper functions
+
+-- | Create a string that defines a table within the GHC session
+--   e.g. testTable = table "testTable" :: Table Person
+createTable :: String -> String
+createTable tType = tableName ++ " = table \"" ++ tableName ++ "\" :: Table " ++ tType
 
 -- | Create a temporary file given a test label and a file extension.
 mkTempFileName :: String -> String -> String
@@ -70,13 +77,12 @@ readFromDatabase = "readFromDatabase"
 
 -- Test input commands
 
--- | Read People items from a pre-existing database.
-input_readFromDatabase :: IO (IORef [String])
-input_readFromDatabase = newIORef [
-    "data People = People {name :: String, age :: Int} deriving (Data, Show, Eq)",
-    "(tPeople, iPeopleName) = (table \"People\" `withIndex` iPeopleName :: Table People, index tPeople \"name\" name :: Index People String)",
-    ":open " ++ mkTempFileName readFromDatabase ".db",
-    "select [x | x <- from tPeople everything]",
+-- | Read items from a pre-existing database.
+input_readFromDatabase ::  String -> [String] ->  IO (IORef [String])
+input_readFromDatabase tableName inputCode = newIORef $ 
+    (":open " ++ mkTempFileName readFromDatabase ".db") : inputCode
+    ++ [
+    "select [x | (_key,x) <- from " ++ tableName ++ " everything]",
     "it",
     ":q"
     ]
@@ -84,22 +90,22 @@ input_readFromDatabase = newIORef [
 -- Tests
 
 -- | Check if the front-end can read from a database.
-prop_readFromDatabase :: [(String, Int)] -> Property
-prop_readFromDatabase testData = ioProperty $ do
+prop_readFromDatabase :: (Arbitrary a, Data a, Show a) => Table a -> [String] ->  [a] -> Property
+prop_readFromDatabase testTable' inputCode testData = ioProperty $ do
     -- Write and retrieve data using the back-end
     (dbFp, db) <- openDB' readFromDatabase
     ref <- runDaison db ReadWriteMode $ do
-        tryDropTable tPeople
-        tryCreateTable tPeople
-        sequence_ $ map (\(s,i) -> insert tPeople (return (People s i))) testData
-        select [x | x <- from tPeople everything]
+        tryDropTable testTable'
+        tryCreateTable testTable'
+        mapM_ (insert_ testTable') testData
+        select [x | (_key,x) <- from testTable' everything]
     closeDB db
 
     -- Retrieve data using the front-end
     stdoutCopy <- hDuplicate stdout
     (fp, fh) <- openTempFile' readFromDatabase
     hDuplicateTo fh stdout
-    cmds <- input_readFromDatabase
+    cmds <- input_readFromDatabase tableName inputCode
     run Nothing $ getNextInput cmds
     hDuplicateTo stdoutCopy stdout
     hClose fh
@@ -109,12 +115,40 @@ prop_readFromDatabase testData = ioProperty $ do
     let res = outputLines !! (length outputLines - 2)
 
     -- Clean up
-    removeFile fp
+    --removeFile fp
     removeFile dbFp
 
-    return $ res == show ref
+    let success = res == show ref
+    unless success $ do
+        print $ "Res: " ++ res
+        print $ "Ref: " ++ show ref
+        
+    return success
+
+-- Arguments for properties
+tPerson = testTable :: Table Person
+tDoubless = testTable :: Table [Double]
+tTuples = testTable :: Table (Bool, Int)
+tChars = testTable :: Table Char
+
+iCPerson = ["data Person = Person {name :: String, age :: Int} deriving (Data, Show)",
+            createTable "Person"
+           ]
+iCDoubless = [createTable "[Double]"]
+iCTuples = [createTable "(Bool,Int)"]
+iCChars = [createTable "Char"]
 
 main :: IO ()
 main = do
-    result <- quickCheckWithResult (stdArgs {maxSuccess = 10}) prop_readFromDatabase
-    unless (isSuccess result) exitFailure
+    print "Reading from database with table :: Table Person"
+    res11 <- quickCheckWithResult stdArgs $ prop_readFromDatabase tPerson iCPerson
+    print "Reading from database with table :: Table [Double]"
+    res12 <- quickCheckWithResult stdArgs $ prop_readFromDatabase tDoubless iCDoubless
+    print "Reading from database with table :: Table (Bool, Int)"
+    res13 <- quickCheckWithResult stdArgs $ prop_readFromDatabase tTuples iCTuples
+    print "Reading from database with table :: Table Char"
+    res14 <- quickCheckWithResult stdArgs $ prop_readFromDatabase tChars iCChars
+
+    let tests = [res11, res12, res13, res14]
+
+    unless (all isSuccess tests) exitFailure
