ebcfca473deaa5ee27eda54240d1592fd08af37e
Christoffer Kalenbrunner <47105299+kaltenbrunner@users.noreply.github.com>
2021-05-26 17:02:19 +0200

    Fix typo and remove comment

--- src/Frontend/GHCInterface.hs
+++ src/Frontend/GHCInterface.hs
@@ -1,6 +1,6 @@
 -- | This is an interface towards the GHC API. Imports from GHC should only be
 -- made in this module. It makes the code clean and all functions from the GHC
--- library will have the infix `GHC.`. This idea is taken from the library
+-- library will have the prefix `GHC.`. This idea is taken from the library
 -- `hint`.
 module Frontend.GHCInterface (module X) where
 
@@ -11,7 +11,7 @@ import HscTypes as X (SourceError, tyThingId)
 import GHC as X
 import GhcMonad as X
 import GHC.LanguageExtensions as X
-import GHC.Paths as X (libdir) -- maybe put this in another module since ghc-paths is a package
+import GHC.Paths as X (libdir)
 import Outputable as X (showSDocForUser, showSDoc, ppr)
 import PprTyThing as X (pprTypeForUser)
 import Pretty as X
